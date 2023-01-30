170eb1d2d15db30d594271960d324e6899eadd53
tommyrunner <1223758238@qq.com>
2023-01-30 16:09:33 +0800

    测试搜索

--- docs/.vuepress/configs/docsearchPlugin.ts
+++ docs/.vuepress/configs/docsearchPlugin.ts
@@ -3,6 +3,9 @@ export default docsearchPlugin({
   appId: 'DLPCCW1SI8',
   apiKey: '8c384f5cdb8b5695865dc839f1ec19cb',
   indexName: 'web-loading',
+  searchParameters: {
+    facetFilters: ['tags:v2']
+  },
   locales: {
     '/': {
       placeholder: '搜索文档',

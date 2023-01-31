7b46a7f114052b42af23887a99b670384d6b1d66
tommyrunner <1223758238@qq.com>
2023-01-31 10:48:35 +0800

    新增中文翻译

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -31,7 +31,22 @@ export default {
         navbar: navbarZh,
         selectLanguageName: '简体中文',
         selectLanguageText: '选择语言',
-        selectLanguageAriaLabel: '选择语言'
+        selectLanguageAriaLabel: '选择语言',
+        // page meta
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdatedText: '上次更新',
+        contributorsText: '贡献者',
+        // custom containers
+        tip: '提示',
+        warning: '注意',
+        danger: '警告',
+        // 404 page
+        notFound: ['这里什么都没有', '我们怎么到这来了？', '这是一个 404 页面', '看起来我们进入了错误的链接'],
+        backToHome: '返回首页',
+        // a11y
+        openInNewWindow: '在新窗口打开',
+        toggleColorMode: '切换颜色模式',
+        toggleSidebar: '切换侧边栏'
       },
       '/us/': {
         sidebar: sidebarUs,

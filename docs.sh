5b6d4c6a41213c1d97e1f1d19161046569f59a75
tommyrunner <1223758238@qq.com>
2025-04-11 09:21:36 +0800

    修复 docs.sh 文件中的注释，删除多余的目录切换和删除命令

--- docs.sh
+++ docs.sh
@@ -11,9 +11,3 @@ git commit -m '更新文档'
 # Push to the gh-page branch of your warehouse
 # Replace<USERNAME>/<REPO>with your information
 git push -f git@github.com:tommyrunner/web-loading.git master:gh-pages
-
-# 返回项目根目录
-cd ..
-
-# 删除 docs/.vuepress/dist 目录
-rm -rf docs/.vuepress/dist
